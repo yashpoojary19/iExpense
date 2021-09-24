1e6bfee82d65b0ecaadee31aa32d4e30ac5e1bda
Yash Poojary <yashspoojary245@gmail.com>
2021-09-24 18:00:34 +0530

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Yash Poojary on 24/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Yash Poojary on 24/09/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
